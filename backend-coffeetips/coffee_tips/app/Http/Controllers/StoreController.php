49cf55ab718c50f353291ec147ef5dbe51ee3264
Bruno Queiroz <bruno.queiroz2011@gmail.com>
2019-07-18 11:34:38 -0300

    Criaçãos do CRUD de Stores

--- backend-coffeetips/coffee_tips/app/Http/Controllers/StoreController.php
+++ backend-coffeetips/coffee_tips/app/Http/Controllers/StoreController.php
@@ -14,9 +14,8 @@ class StoreController extends Controller
      */
     public function index()
     {
-        echo "Teste";
         $store = Store::all();
-        return $store->toJson();
+        return $store;
     }
 
     /**
@@ -37,7 +36,34 @@ class StoreController extends Controller
      */
     public function store(Request $request)
     {
-        //
+        $store = new Store();
+        $store->name                = $request->input('name');
+        $store->email               = $request->input('email');
+        $store->password            = bcrypt($request->input('password'));
+        $store->description         = $request->input('description');
+        $store->neighborhood        = $request->input('neighborhood');
+        $store->number              = $request->input('number');
+        $store->city                = $request->input('city');
+        $store->state               = $request->input('state');
+        $store->cep                 = $request->input('cep');
+        $store->country             = $request->input('country');
+        $store->street              = $request->input('street');
+        $store->schedule            = $request->input('schedule');
+        $store->DDD1                = $request->input('DDD1');
+        $store->telephone           = $request->input('telephone');
+        $store->DDD2                = $request->input('DDD2');
+        $store->cellphone           = $request->input('cellphone');
+        $store->instagram           = $request->input('instagram');
+        $store->facebook            = $request->input('facebook');
+        $store->logo                = $request->input('logo');
+        $store->firdt_img_local     = $request->input('firdt_img_local');
+        $store->second_img_local    = $request->input('second_img_local');
+        $store->third_img_local     = $request->input('third_img_local');
+        $store->save();
+        return response()->json([
+            'error' => false,
+            'message' => 'Cafeteria cadastrada com sucesso!'
+        ]);
     }
 
     /**
@@ -49,7 +75,7 @@ class StoreController extends Controller
     public function show($id)
     {
         $store = Store::find($id);
-        return $store->toJson();
+        return $store;
     }
 
     /**
@@ -72,7 +98,36 @@ class StoreController extends Controller
      */
     public function update(Request $request, $id)
     {
-        //
+        $store = new Store();
+        if (isset($store)){
+            $store->name                = $request->input('name');
+            $store->email               = $request->input('email');
+            $store->password            = bcrypt($request->input('password'));
+            $store->description         = $request->input('description');
+            $store->neighborhood        = $request->input('neighborhood');
+            $store->number              = $request->input('number');
+            $store->city                = $request->input('city');
+            $store->state               = $request->input('state');
+            $store->cep                 = $request->input('cep');
+            $store->country             = $request->input('country');
+            $store->street              = $request->input('street');
+            $store->schedule            = $request->input('schedule');
+            $store->DDD1                = $request->input('DDD1');
+            $store->telephone           = $request->input('telephone');
+            $store->DDD2                = $request->input('DDD2');
+            $store->cellphone           = $request->input('cellphone');
+            $store->instagram           = $request->input('instagram');
+            $store->facebook            = $request->input('facebook');
+            $store->logo                = $request->input('logo');
+            $store->firdt_img_local     = $request->input('firdt_img_local');
+            $store->second_img_local    = $request->input('second_img_local');
+            $store->third_img_local     = $request->input('third_img_local');
+            $store->save();
+        }
+        return response()->json([
+            'error' => false,
+            'message' => 'Cafeteria atualizada com sucesso!'
+        ]);
     }
 
     /**
@@ -83,6 +138,13 @@ class StoreController extends Controller
      */
     public function destroy($id)
     {
-        //
+        $store = Store::find($id);
+        if (isset($store)){
+            $store->delete();
+        }
+        return response()->json([
+            'error' => false,
+            'message' => 'Cafeteria excluida com sucesso!'
+        ]);
     }
 }
